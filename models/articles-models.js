c0d69fa26ce760b1ba5f476d54d27dbb704b05c2
Gemma Baker <gemmabaker@Gemmas-MacBook-Pro.local>
2024-11-04 14:11:54 +0000

    fixed missing body

--- models/articles-models.js
+++ models/articles-models.js
@@ -4,7 +4,7 @@ const format = require("pg-format");
 function selectArticleById(articleId) {
   return db
     .query(
-      `SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, count(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
+      `SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, articles.body, count(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
       [articleId]
     )
     .then((result) => {
